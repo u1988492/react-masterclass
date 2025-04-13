75a94e37c9d8521e3bb9e130e7fed1fb8c1a3e4d
Claudia <149967204+claudiahodoroga@users.noreply.github.com>
2025-04-13 19:07:06 +0200

    finished!

--- react-app-masterclass/src/App.tsx
+++ react-app-masterclass/src/App.tsx
@@ -1,8 +1,20 @@
-import Message from "./Message";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
-function App(){
-  const name = 'Claudia';
-  return <div><Message/></div>;
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          This is an alert
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ react-app-masterclass/src/components/ListGroup.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIdx, setSelectedIdx] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group ">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIdx === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIdx(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
